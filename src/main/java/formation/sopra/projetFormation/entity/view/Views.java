eccf5814d59fd286b4fcdd5302a881341e05b6f3
valentin <valijoubs@gmail.com>
2020-06-01 10:07:37 +0200

    modif view

--- src/main/java/formation/sopra/projetFormation/entity/view/Views.java
+++ src/main/java/formation/sopra/projetFormation/entity/view/Views.java
@@ -1,16 +1,34 @@
 package formation.sopra.projetFormation.entity.view;
 
+
 public class Views {
 	// niveau de base
 	public static class Common {
 
 	}
-	
+
 //	public static class Delete extends Common {
 //
 //	}
-	
+
 	public static class AnnonceByPersonne extends Common {
+
+	}
+
+	public static class ChienWithAnnonce extends Common {
+
+	}
+	
+	public static class AnnonceWithChien extends Common {
 		
 	}
+
+	public static class ChienWithPersonne extends Common {
+	
+	}
+	
+	public static class PersonneWithChien extends Common {
+		
+	}
+
 }
